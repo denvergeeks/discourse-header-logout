c7bcc0032a88cf89f625dc64380894772ec962f2
denvergeeks <jen@denvergeeks.com>
2024-05-22 07:02:10 -0600

    Update todo.js

--- javascripts/discourse/api-initializers/todo.js
+++ javascripts/discourse/api-initializers/todo.js
@@ -1,5 +1,7 @@
-import { apiInitializer } from "discourse/lib/api";
+import LogoutHeaderButton from "../components/logout-header-button";
 
-export default apiInitializer("1.8.0", (/* api */) => {
-  // console.log("hello world from api initializer!");
+export default apiInitializer("1.28.0", (api) => {
+  if (api.getCurrentUser()) {
+    api.headerIcons.add("logout", LogoutHeaderButton, { before: "auth" });
+  }
 });
